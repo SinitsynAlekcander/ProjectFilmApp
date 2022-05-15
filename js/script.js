9e53ef631f0718fcc43bb8ede6fefc2e355dbc10
SinitsynAlekcander <sinitsynalekcander@gmail.com>
2022-05-15 12:03:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict"; //Директива для современного кода, где неработают прошлые некоторые неточности
+
+// a=10;
+// console.log(a);
+
+
+console.log('Hello');
+
+// ПЕРЕМЕННЫЕ
+let number = 5; //изменяемая
+const nameMe = 'Al'; //НЕ изменяемая, нельзя переписать
+
+number = 12;
+console.log(nameMe);
+
+// const me = {
+//     name: 'Al',
+//     age: 34
+// };
+// console.log(me.name);
+// console.log(me['age']);
+
+const arr = [5, 4, 3, 2];
+console.log(arr[0]);
+
+const arrObj = {
+a: 'a',
+b: 'b',
+c: 'c'
+};
+
+console.log(arrObj.c);
